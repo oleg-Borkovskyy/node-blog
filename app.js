b51b15ec710f10484f30ee108b90033a8d60903c
Oleg Borkovskyi <58165566+oleg-Borkovskyy@users.noreply.github.com>
2024-02-13 12:19:02 +0200

    create server folder and routes

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 require("dotenv").config()
 
 const express = require("express")
+const expressLayout = require("express-ejs-layouts")
 
 const app = express()
 const PORT= 5000 || process.env.PORT
+
+app.use(express.static('public'))
+
+// templating Engine
+app.use(expressLayout)
+app.set('layout','/layouts/main')
+app.set('view engine', 'ejs')
+
+app.use('/', require('./server/routes/main'))
 
 app.get("", (req, res) => {
     res.send('hello world')
